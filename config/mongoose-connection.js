430013983a2607b64ab0b9ef48dedd1339550e70
khushii018 <khushimahto1@gmail.com>
2025-08-23 23:43:00 +0530

    updating

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,20 +1,34 @@
-const mongoose=require('mongoose');
-const config=require("config");
+// const mongoose=require('mongoose');
+// const config=require("config");
 
-const dbgr = require("debug")("development:mongoose");
+// const dbgr = require("debug")("development:mongoose");
 
 
-mongoose
-.connect(`${config.get("MONGODB_URI")}/perfume-hub`)
-.then(function(){
-    dbgr("connected to db");
+// mongoose
+// .connect(`${config.get("MONGODB_URI")}/perfume-hub`)
+// .then(function(){
+//     dbgr("connected to db");
 
 
     
 
-})
-.catch(function(err){
-    dbgr(err);
-});
+// })
+// .catch(function(err){
+//     dbgr(err);
+// });
 
-module.exports= mongoose.connection;
+// module.exports= mongoose.connection;
+const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
+
+const mongoURI = process.env.MONGODB_URI; // Read directly from environment
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => dbgr("Connected to MongoDB"))
+  .catch((err) => dbgr("MongoDB connection error:", err));
+
+module.exports = mongoose.connection;
